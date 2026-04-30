d0b4d1f00c5dea56565dd2af6a9194a0acbbb378
Samuelife-techbro <adojutelegansamuel263@gmail.com>
2026-04-30 23:23:05 +0100

    fix database with sqlite

--- backend/hotel_project/settings.py
+++ backend/hotel_project/settings.py
@@ -76,13 +76,16 @@ ASGI_APPLICATION = 'hotel_project.asgi.application'
 
 # Database - PostgreSQL
 DATABASES = {
+   # 'default': {
+       # 'ENGINE': 'django.db.backends.postgresql',
+        #'NAME': os.environ.get('DB_NAME', 'hotel_db'),
+       # 'USER': os.environ.get('DB_USER', 'postgres'),
+       # 'PASSWORD': os.environ.get('DB_PASSWORD', 'postgres'),
+        #'HOST': os.environ.get('DB_HOST', 'localhost'),
+        #'PORT': os.environ.get('DB_PORT', '5432'), }
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': os.environ.get('DB_NAME', 'hotel_db'),
-        'USER': os.environ.get('DB_USER', 'postgres'),
-        'PASSWORD': os.environ.get('DB_PASSWORD', 'postgres'),
-        'HOST': os.environ.get('DB_HOST', 'localhost'),
-        'PORT': os.environ.get('DB_PORT', '5432'),
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
